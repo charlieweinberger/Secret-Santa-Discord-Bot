cb4befe0e2dce900407b5ea89ca5711727b6e96a
Charlie Weinberger <364charliebrown@gmail.com>
2022-05-14 17:21:50 -0700

    got rid of commands.js (1/2)

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import DiscordJS, { Intents } from 'discord.js';
 import dotenv from 'dotenv';
 dotenv.config();
-import { allCommands } from './commands.js';
 
 const client = new DiscordJS.Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
@@ -15,15 +14,33 @@ client.on('ready', () => {
     const guild = client.guilds.cache.get(guildId);
     let commands = guild ? guild.commands : client.application?.commands;
 
-    for (const command of allCommands) {
+    commands?.create({
+        name: 'hello',
+        description: 'says hi!'
+    });
 
-        commands?.create({
-            name: command.name,
-            description: command.description,
-            options: command.options
-        });
-
-    }
+    commands?.create({
+        name: 'add',
+        description: 'adds numbers',
+        options: [
+            {
+                name: 'num1',
+                description: 'The first number.',
+                required: true,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+            }, {
+                name: 'num2',
+                description: 'The second number.',
+                required: true,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+            }, {
+                name: 'num3',
+                description: 'The third number.',
+                required: false,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+            }
+        ]
+    });
 
 });
 
@@ -33,8 +50,23 @@ client.on('interactionCreate', async (interaction) => {
 
     const {commandName, options } = interaction;
 
-    allCommands.filter(command => commandName === command.name)[0]
-               .do(interaction, options);
+    if (commandName === 'hello') {
+
+        interaction.reply({
+            content: 'hi'
+        });
+
+    } else if (commandName === 'add') {
+
+        const num1 = options.getNumber('num1') || 0;
+        const num2 = options.getNumber('num2') || 0;
+        const num3 = options.getNumber('num3') || 0;
+
+        interaction.reply({
+            content: `The sum is ${num1 + num2 + num3}`
+        });
+
+    }
 
 });
 
