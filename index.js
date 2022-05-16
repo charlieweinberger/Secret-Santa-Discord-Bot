460e5dedea538ac1923f612d3aee37fb5fdf3a19
Charlie Weinberger <364charliebrown@gmail.com>
2022-05-16 01:30:40 -0700

    added reaction roles (still needs improvements)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Client, Intents, Constants, MessageEmbed } from 'discord.js';
+import { Client, Intents, Constants, MessageEmbed, MessageActionRow, MessageButton } from 'discord.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -9,12 +9,14 @@ const client = new Client({
     ]
 });
 
+let guild;
+let roles = [];
+
 client.on('ready', () => {
 
     console.log('Bot is ready!');
 
-    const guildId = '849914162216960021';
-    const guild = client.guilds.cache.get(guildId);
+    guild = client.guilds.cache.get('849914162216960021');
     const commands = guild ? guild.commands : client.application?.commands;
 
     commands.create({
@@ -90,18 +92,13 @@ client.on('ready', () => {
                 required: true,
                 type: Constants.ApplicationCommandOptionTypes.STRING
             }, {
-                name: 'emoji-1',
-                description: 'The first emoji.',
-                required: true,
-                type: Constants.ApplicationCommandOptionTypes.STRING
-            }, {
                 name: 'role-1',
                 description: 'The first role.',
                 required: true,
                 type: Constants.ApplicationCommandOptionTypes.ROLE
             }, {
-                name: 'emoji-2',
-                description: 'The second emoji.',
+                name: 'description',
+                description: 'The description of the reaction role embed.',
                 required: false,
                 type: Constants.ApplicationCommandOptionTypes.STRING
             }, {
@@ -117,53 +114,84 @@ client.on('ready', () => {
 
 client.on('interactionCreate', async (interaction) => {
 
-    if (!interaction.isCommand()) return;
+    if (interaction.isCommand()) {
 
-    const {commandName, options } = interaction;
+        const { commandName, options } = interaction;
 
-    if (commandName === 'hello') {
+        if (commandName === 'hello') {
 
-        interaction.reply({
-            content: 'hi'
-        });
+            interaction.reply({
+                content: 'hi'
+            });
 
-    } else if (commandName === 'add') {
+        } else if (commandName === 'add') {
 
-        const num1 = options.getNumber('num-1');
-        const num2 = options.getNumber('num-2');
-        const num3 = options.getNumber('num-3');
-        const num4 = options.getNumber('num-4');
-        const num5 = options.getNumber('num-5');
-        const num6 = options.getNumber('num-6');
-        const num7 = options.getNumber('num-7');
-        const num8 = options.getNumber('num-8');
-        const num9 = options.getNumber('num-9');
-        const num10 = options.getNumber('num-10');
+            const num1 = options.getNumber('num-1');
+            const num2 = options.getNumber('num-2');
+            const num3 = options.getNumber('num-3');
+            const num4 = options.getNumber('num-4');
+            const num5 = options.getNumber('num-5');
+            const num6 = options.getNumber('num-6');
+            const num7 = options.getNumber('num-7');
+            const num8 = options.getNumber('num-8');
+            const num9 = options.getNumber('num-9');
+            const num10 = options.getNumber('num-10');
 
-        interaction.reply({
-            content: `The sum is ${num1 + num2 + num3 + num4 + num5 +
-                                   num6 + num7 + num8 + num9 + num10}`
-        });
+            interaction.reply({
+                content: `The sum is ${num1 + num2 + num3 + num4 + num5 +
+                                    num6 + num7 + num8 + num9 + num10}`
+            });
 
-    } else if (commandName === 'rr') {
+        } else if (commandName === 'rr') {
+            
+            const title = options.getString('title');
+            const description = options.getString('description');
+            
+            roles = [
+                options.getRole('role-1'),
+                options.getRole('role-2')
+            ];
+
+            const rrEmbed = new MessageEmbed()
+                .setTitle(title)
+                .setDescription(description);
+
+            const row = new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                        .setCustomId(`rr-role-1`)
+                        .setLabel(roles[0].name)
+                        .setStyle('PRIMARY'),
+                    new MessageButton()
+                        .setCustomId(`rr-role-2`)
+                        .setLabel(roles[1].name)
+                        .setStyle('PRIMARY')
+                )
+
+            interaction.reply({
+                embeds: [rrEmbed],
+                components: [row]
+            });
+
+        }
+
+    } else if (interaction.isButton()) {
         
-        const title  = options.getString('title');
-        const emoji1 = options.getString('emoji-1');
-        const emoji2 = options.getString('emoji-2');
+        const member = guild.members.cache.get(interaction.user.id);
 
-        const role1 = options.getRole('role-1');
-        const role2 = options.getRole('role-2');
-        
-        const rrEmbed = new MessageEmbed()
-            .setTitle(title)
-            .setDescription(`
-                ${emoji1}: ${role1}\n
-                ${emoji2}: ${role2}
-            `);
-
-        interaction.reply({
-            embeds: [rrEmbed]
-        });
+        if (interaction.customId === 'rr-role-1') {
+            member.roles.add(roles[0]);
+            interaction.reply({
+                content: `Successfully added ${roles[0].name} role`,
+                ephemeral: true
+            });
+        } else if (interaction.customId === 'rr-role-2') {
+            member.roles.add(roles[1]);
+            interaction.reply({
+                content: `Successfully added ${roles[1].name} role`,
+                ephemeral: true
+            });
+        }
 
     }
 
